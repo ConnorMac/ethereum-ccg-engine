87b3f1c45fcd81dc09b2eb5f20f5f2ce80886c8e
ConnorMac <cmacdougall78@gmail.com>
2018-01-22 20:59:09 +0200

    Set only 1 legendary per pack in example

--- contracts/ExampleCcg.sol
+++ contracts/ExampleCcg.sol
@@ -4,6 +4,7 @@ import "./Ccg.sol";
 contract ExampleCcg is Ccg {
     string[] public Rarity = ["common", "rare", "epic", "legendary"];
     uint[5] public packDisplay;
+    uint public packSize = 5;
 
     function purchasePack() public payable {
         uint[5] memory pack = ExampleCcg._generatePack();
@@ -18,10 +19,28 @@ contract ExampleCcg is Ccg {
         Uses a 0-9 Pseudo random number generator to calc rarity:
         9 - Legendary
         7,8 = Epic
-        5,6,7 = Rare
-        0,1,2,3,4 = Common
+        4,5,6 = Rare
+        0,1,2,3 = Common
         */
-        uint[5] memory pack = Ccg._generateRandomNumberSet(10, 5);
-        return pack;
+        uint[5] memory rarityIterations;
+        bytes32 blockHash = block.blockhash(block.number-1);
+        for (uint index = 0; index < packSize; index++) {
+            uint randomNum = uint(blockHash) % 10;
+            blockHash = keccak256(blockHash);
+            uint legendCount = 0;
+            uint epicCount = 0;
+            uint rareCount = 0;
+            // uint memory common_count = 0;
+            for (uint x = 0; x < rarityIterations.length; x++) {
+                if (rarityIterations[x] == 9) {
+                    if (legendCount >= 1) {index--;}
+                    legendCount++;
+                }
+                if (rarityIterations[x] == 7 || rarityIterations[x] == 8) {epicCount++;}
+                if (rarityIterations[x] == 5 || rarityIterations[x] == 6 || rarityIterations[x] == 4) {rareCount++;}
+            }
+            rarityIterations[index] = randomNum;
+        }
+        return rarityIterations;
     }
 }
